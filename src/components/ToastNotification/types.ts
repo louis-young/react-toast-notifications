6db13b8cce76a8e1bda4c5aaed9bd92b590665ae
Louis Young <me@louisyoung.co.uk>
2021-11-18 07:45:35 +0000

    Update types

--- src/components/ToastNotification/types.ts
+++ src/components/ToastNotification/types.ts
@@ -2,5 +2,5 @@ import { ToastNotification } from "../../context/toastNotifications/types";
 
 export interface ToastNotificationProps {
   text: ToastNotification["text"];
-  deleteToastNotification: ({ id }: { id: ToastNotification["id"] }) => void;
+  deleteToastNotification: () => void;
 }
